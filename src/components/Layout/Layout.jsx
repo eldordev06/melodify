e8d258d47c92e5baf88e915fc210cf3556caf35f
Eldor <eldorforprogramming@gmail.com>
2024-04-23 15:55:40 +0500

    font properties fixed

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -5,7 +5,7 @@ import Player from "./Player";
 
 export default function Layout({ children }) {
   return (
-    <div className="flex flex-col h-screen w-screen">
+    <div className="flex flex-col h-screen w-screen text-xl font-medium">
       <div className="flex flex-grow">
         <Sidebar />
         <div className="flex-grow h-full">
