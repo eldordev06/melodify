ac0ad44f0ee6c5a45ed66a423eeaa742a3441ae1
Eldor <eldorforprogramming@gmail.com>
2024-04-23 15:49:11 +0500

    player height fixed

--- src/components/Layout/Player.jsx
+++ src/components/Layout/Player.jsx
@@ -1,7 +1,7 @@
 export default function Player() {
   return (
     <>
-      <div className="bg-[#282828] text-white h-24 text-xl flex items-center justify-center">
+      <div className="bg-[#282828] text-white h-24 text-xl flex-shrink-0 flex items-center justify-center">
         <span>PLAYER</span>
       </div>
     </>
