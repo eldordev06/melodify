0a89a39e2192ea7761bf2cdfe8631763cceb0cd2
Eldor <eldorforprogramming@gmail.com>
2024-04-23 16:07:27 +0500

    sidebar height fixed

--- src/components/Layout/Sidebar.jsx
+++ src/components/Layout/Sidebar.jsx
@@ -2,35 +2,41 @@ import SidebarItem from "../SidebarItem";
 
 export default function Sidebar() {
   return (
-    <aside className="bg-black w-[300px] h-full text-white px-2.5">
-      <a href="https://open.spotify.com" target="_blank" className="block p-5">
-        <img
-          src="/public/icons/sidebar/logo.svg"
-          alt="Logo."
-          className="w-[165px] h-[50px] object-contain"
-        />
-      </a>
-      <ul className="mb-[52px]">
-        <SidebarItem itemName="home" iconName="home" isActive={true} />
-        <SidebarItem itemName="search" iconName="search" />
-        <SidebarItem itemName="library" iconName="library" />
-      </ul>
-      <ul className="mb-10">
-        <SidebarItem itemName="create-playlist" iconName="create-playlist" />
-        <SidebarItem itemName="liked-songs" iconName="liked-songs" />
-        <SidebarItem itemName="your-episodes" iconName="your-episodes" />
-      </ul>
-      <ul>
-        <SidebarItem itemName="FAV" />
-        <SidebarItem itemName="Daily Mix 1" />
-        <SidebarItem itemName="Discover Weekly" />
-        <SidebarItem itemName="Malayalam" />
-        <SidebarItem itemName="Dance / Electronix Mix" />
-        <SidebarItem itemName="EDM / Popular" />
-      </ul>
-      <a href="/download" className="mt-16 mb-3.5">
-        <SidebarItem itemName="Install App" iconName="arrow-down" />
-      </a>
+    <aside className="bg-black w-[300px] h-full text-white px-2.5 relative">
+      <div className="absolute top-0 left-0 right-0 bottom-0 overflow-auto">
+        <a
+          href="https://open.spotify.com"
+          target="_blank"
+          className="block p-5"
+        >
+          <img
+            src="/public/icons/sidebar/logo.svg"
+            alt="Logo."
+            className="w-[165px] h-[50px] object-contain"
+          />
+        </a>
+        <ul className="mb-[52px]">
+          <SidebarItem itemName="home" iconName="home" isActive={true} />
+          <SidebarItem itemName="search" iconName="search" />
+          <SidebarItem itemName="library" iconName="library" />
+        </ul>
+        <ul className="mb-10">
+          <SidebarItem itemName="create-playlist" iconName="create-playlist" />
+          <SidebarItem itemName="liked-songs" iconName="liked-songs" />
+          <SidebarItem itemName="your-episodes" iconName="your-episodes" />
+        </ul>
+        <ul>
+          <SidebarItem itemName="FAV" />
+          <SidebarItem itemName="Daily Mix 1" />
+          <SidebarItem itemName="Discover Weekly" />
+          <SidebarItem itemName="Malayalam" />
+          <SidebarItem itemName="Dance / Electronix Mix" />
+          <SidebarItem itemName="EDM / Popular" />
+        </ul>
+        <a href="/download" className="mt-16 mb-3.5">
+          <SidebarItem itemName="Install App" iconName="arrow-down" />
+        </a>
+      </div>
     </aside>
   );
 }
