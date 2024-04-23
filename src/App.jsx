c2ff9faa7b31051db348831ff11e414b7775faac
Eldor <eldorforprogramming@gmail.com>
2024-04-23 18:45:42 +0500

    Rec component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Layout from "./components/Layout/Layout";
-import Playlist from "./components/Playlist";
+import Rec from "./components/Rec";
 import RecItem from "./components/RecItem";
 
 function App() {
@@ -8,31 +8,7 @@ function App() {
       <Layout>
         <main className="flex-grow relative">
           <div className="absolute top-0 left-0 right-0 bottom-0 overflow-auto px-10 pb-10">
-            <section className="mt-[50px]">
-              <h1 className="text-4xl mb-7">Good morning</h1>
-              <div className="grid grid-cols-4 gap-5">
-                <Playlist
-                  iconPath="/public/icons/sidebar/liked-songs.svg"
-                  plName="Liked Songs"
-                />
-                <Playlist
-                  iconPath="/public/icons/sidebar/liked-songs.svg"
-                  plName="Neffex Playlist"
-                />
-                <Playlist
-                  iconPath="/public/icons/sidebar/liked-songs.svg"
-                  plName="K/DA"
-                />
-                <Playlist
-                  iconPath="/public/icons/sidebar/liked-songs.svg"
-                  plName="Liked Songs"
-                />
-                <Playlist
-                  iconPath="/public/icons/sidebar/liked-songs.svg"
-                  plName="Dance / Electronic Mix"
-                />
-              </div>
-            </section>
+            <Rec heading="Good morning" />
             <section className="pt-[50px]">
               <h2 className="text-3xl mb-7">Shows you might like</h2>
               <div className="flex flex-wrap gap-[30px]">
---
+++ src/components/Rec.jsx
@@ -0,0 +1,32 @@
+/* eslint-disable react/prop-types */
+import Playlist from "./Playlist";
+
+export default function Rec({ heading }) {
+  return (
+    <section className="mt-[50px]">
+      <h1 className="text-4xl mb-7">{heading}</h1>
+      <div className="grid grid-cols-4 gap-5">
+        <Playlist
+          iconPath="/public/icons/sidebar/liked-songs.svg"
+          plName="Liked Songs"
+        />
+        <Playlist
+          iconPath="/public/icons/sidebar/liked-songs.svg"
+          plName="Neffex Playlist"
+        />
+        <Playlist
+          iconPath="/public/icons/sidebar/liked-songs.svg"
+          plName="K/DA"
+        />
+        <Playlist
+          iconPath="/public/icons/sidebar/liked-songs.svg"
+          plName="Liked Songs"
+        />
+        <Playlist
+          iconPath="/public/icons/sidebar/liked-songs.svg"
+          plName="Dance / Electronic Mix"
+        />
+      </div>
+    </section>
+  );
+}
