f97769c2d411435544b1c035dafa9f76cca4339a
Eldor <eldorforprogramming@gmail.com>
2024-04-23 18:23:41 +0500

    img style fixed

--- src/components/Playlist.jsx
+++ src/components/Playlist.jsx
@@ -2,7 +2,7 @@
 export default function Playlist({ iconPath, plName }) {
   return (
     <div className="flex items-center gap-5 bg-[#303030] rounded h-[100px] cursor-pointer transition hover:bg-[#3f3f3f]">
-      <img src={iconPath} alt="Heart icon." className="h-full object-cover" />
+      <img src={iconPath} alt="Heart icon." className="h-full aspect-square object-cover" />
       <span>{plName}</span>
     </div>
   );
