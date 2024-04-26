731ec579d355d6938d257cdc1c5c138924e1021d
Eldor <eldorforprogramming@gmail.com>
2024-04-26 12:19:19 +0500

    layout structure fixed

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -11,7 +11,11 @@ export default function Layout() {
         <Sidebar />
         <div className="flex-grow h-full flex flex-col">
           <Header />
-          <Outlet />
+          <main className="flex-grow relative">
+            <div className="absolute top-0 left-0 right-0 bottom-0 overflow-auto px-10 pb-10 [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">
+              <Outlet />
+            </div>
+          </main>
         </div>
       </div>
       <Player />
